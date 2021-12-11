62af5f1fb8122f5d96535f8c84a85e4d78ae012c
maksimellian <maksimellian01@yandex.ru>
2021-12-11 06:55:16 +0300

    Scripted auto-commit on change (2021-12-11 06:55:16) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -1,10 +1,19 @@
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class AirportReducer extends Reducer<FlightWritableComparable, Text, IntWritable, Text> {
     @Override
-    protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) {
-
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
+            IOException, InterruptedException {
+        long count = 0;
+        while (values.iterator().hasNext()) {
+            values.iterator().next();
+            count += 1;
+        }
+        context.write(key, new LongWritable(count));
     }
 }
