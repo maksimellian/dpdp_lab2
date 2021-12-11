b73947e2551ea7fca9a62dd73f608de7d8a89a69
maksimellian <maksimellian01@yandex.ru>
2021-12-11 04:27:56 +0300

    Scripted auto-commit on change (2021-12-11 04:27:56) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    
+    @Override
+    protected void map
 }
