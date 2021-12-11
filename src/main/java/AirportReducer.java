d0f6423171c356eb2fef0c00c6a347329f5838e7
maksimellian <maksimellian01@yandex.ru>
2021-12-11 06:57:02 +0300

    Scripted auto-commit on change (2021-12-11 06:57:02) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -4,7 +4,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-// 
+// Разрабатываем reduce функцию, которая берет первую строку, извлекает из
+// нее имя аэропорта, далее рассчитывает из последующих строк среднее
+// минимальное и максимальное время задержки и печатает результат.
 public class AirportReducer extends Reducer<FlightWritableComparable, Text, IntWritable, Text> {
 
     @Override
