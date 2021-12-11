ab9ca823e74b4ddcea62fbd8b6362a5ea45156d9
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:01:56 +0300

    Scripted auto-commit on change (2021-12-11 05:01:56) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, IntWritable> {
     public static final float CANCELLED = 1.0f;
     public static final int AIRPORT_ID = 14; // номер колонки id аэропорта
+    public static final int DELAY = 18; // номер колонки задержки
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
