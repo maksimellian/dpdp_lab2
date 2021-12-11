a8675dc6a065acf297e1a4e6012af9e1d7fe6116
maksimellian <maksimellian01@yandex.ru>
2021-12-11 04:26:33 +0300

    Scripted auto-commit on change (2021-12-11 04:26:33) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -2,5 +2,5 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper<LongWritable, Text> {
+public class FlightMapper extends Mapper<LongWritable, Text, > {
 }
