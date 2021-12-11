bf0018ca1b20d36d3bac55f5a837a12e269bbd69
maksimellian <maksimellian01@yandex.ru>
2021-12-11 04:09:04 +0300

    Scripted auto-commit on change (2021-12-11 04:09:04) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class AirportMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportMapper {
+
+
 }
