7acc174272481ec6f83b1cd41d5598f3029dc49f
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:49:17 +0300

    Scripted auto-commit on change (2021-12-11 05:49:17) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -3,7 +3,14 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     public static final int AIRPORT_ID = 0;
     public static final int DESCRIPTION = 1;
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+        
+    }
 }
