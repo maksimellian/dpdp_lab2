ef693185919b716cb825ee56b7f9fb4cd6ad6a00
maksimellian <maksimellian01@yandex.ru>
2021-12-11 06:08:10 +0300

    Scripted auto-commit on change (2021-12-11 06:08:10) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     public static final int AIRPORT_ID = 0;
     public static final int DESCRIPTION = 1;
+    public static final int INDICATOR = 0;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
