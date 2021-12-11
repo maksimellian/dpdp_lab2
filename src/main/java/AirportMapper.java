773d5970da154784697f43d6d9538ac4f1422fd9
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:48:16 +0300

    Scripted auto-commit on change (2021-12-11 05:48:16) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -4,5 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    public static final int 
+    public static final int AIRPORT_ID = 0;
+    public static final int  = 1;
 }
