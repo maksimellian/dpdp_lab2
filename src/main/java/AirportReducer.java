78d9092506d36788baeaa097a9a4587c9cd513ef
maksimellian <maksimellian01@yandex.ru>
2021-12-11 06:51:29 +0300

    Scripted auto-commit on change (2021-12-11 06:51:29) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -1,4 +1,5 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
+public class AirportReducer extends Reducer<FlightWritableComparable, Text, IntWritable, Text> {
 }
