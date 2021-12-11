d46eba64b3e31188d3a3e9fb9c66e2d496d33895
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:49:31 +0300

    Scripted auto-commit on change (2021-12-11 05:49:31) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -11,6 +11,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String[]
+        String[] airports = 
     }
 }
