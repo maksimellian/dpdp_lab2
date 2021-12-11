e3db235fafab70d92e9c62cb3dd048879be9cd05
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:33:16 +0300

    Scripted auto-commit on change (2021-12-11 05:33:16) by gitwatch.sh

--- src/main/java/FlightWritableComparable.java
+++ src/main/java/FlightWritableComparable.java
@@ -21,11 +21,11 @@ public class FlightWritableComparable implements WritableComparable<FlightWritab
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         dataOutput.writeInt(airportID);
-        dataOutput.writeInt();
+        dataOutput.writeInt(indicator);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        
     }
 }
