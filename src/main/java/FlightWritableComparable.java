6a4c8789904417d48cca874c857346c69e14d61e
maksimellian <maksimellian01@yandex.ru>
2021-12-11 05:27:54 +0300

    Scripted auto-commit on change (2021-12-11 05:27:54) by gitwatch.sh

--- src/main/java/FlightWritableComparable.java
+++ src/main/java/FlightWritableComparable.java
@@ -1,8 +1,29 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class FlightWritableComparable implements WritableComparable<FlightWritableComparable> {
+    int airportID;
+    int indicator;
     public FlightWritableComparable(int airportID, int indicator) {
         super();
         this.
+    }
+
+    @Override
+    public int compareTo(FlightWritableComparable flightWritableComparable) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
     }
 }
