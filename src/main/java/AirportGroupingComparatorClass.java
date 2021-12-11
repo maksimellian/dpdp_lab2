29875f847b8daf1f4cd828b8ac8652dc4ce6f793
maksimellian <maksimellian01@yandex.ru>
2021-12-11 06:36:21 +0300

    Scripted auto-commit on change (2021-12-11 06:36:21) by gitwatch.sh

--- src/main/java/AirportGroupingComparatorClass.java
+++ src/main/java/AirportGroupingComparatorClass.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
+// 
 public class AirportGroupingComparatorClass extends WritableComparator{
     public AirportGroupingComparatorClass() {
         super(FlightWritableComparable.class, true);
